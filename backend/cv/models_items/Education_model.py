e64573791b7ac5d2a9062e4a3c0a5a0746111106
Ramin <yazdani76ramin@gmail.com>
2024-05-06 20:57:50 +0330

    Ramin | My CV Website | 2024-05-06 20:57 | feature/feature-backend | Feat: Add Education model to link education records to users
    - Created Education model to manage user education details within the database.
    - The model includes a ForeignKey to auth.User, establishing a relational link that ensures each education record is associated with a specific user.
    - CASCADE deletion policy is set, meaning deleting a user will also remove their associated education records.
    - This model addition aims to facilitate easy management and retrieval of user education details across the application.

---
+++ backend/cv/models_items/Education_model.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Education(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='educations')
+
+    def __str__(self):
+        return f"Education Record for {self.user.username}"
