d97e1253ad5c2ef6893b7040a44155176836aca9
Ramin <yazdani76ramin@gmail.com>
2024-05-07 14:36:10 +0330

    Ramin | My CV Website | 2024-05-07 14:36 | feature/feature-backend | Feat: Create ProjectItems model
    This commit adds the ProjectItems model, designed to detail components of each project:
    
    - Directly linked to the Projects model through a ForeignKey, ensuring each project item is associated with the correct project.
    - Includes detailed attributes like title, description, start and end dates, and URL, enabling a thorough documentation of project activities.
    - Facilitates better organization and presentation of specific project elements, improving the overall project management within the CV website.

---
+++ backend/cv/models_items/ProjectItems_model.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class ProjectItems(models.Model):
+    projects = models.ForeignKey('Projects', on_delete=models.CASCADE, related_name='project_items')
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    url = models.CharField(max_length=255)
+    project_file = models.IntegerField()  # assuming project_file references an ID in another table
+
+    def __str__(self):
+        return self.title
