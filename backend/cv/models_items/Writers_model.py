1ed862bca44939a97c6ffee30c8ccb597c6c4c60
Ramin <yazdani76ramin@gmail.com>
2024-05-07 14:20:33 +0330

    Ramin | My CV Website | 2024-05-07 14:20 | feature/feature-backend | Feat: Create Writers model to manage author data
    Added the Writers model to handle comprehensive data about authors involved in various publications:
    
    - Includes personal information fields like first name, last name, and institutional affiliation.
    - Links each writer to their literatures through a foreign key, allowing tracking of all works associated with a particular author.
    - Captures additional metadata like writer type and writer number to differentiate between different roles or identifiers.

---
+++ backend/cv/models_items/Writers_model.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Writers(models.Model):
+    id = models.AutoField(primary_key=True)
+    litrature = models.ForeignKey('Litratures', on_delete=models.CASCADE, related_name='writers')
+    abbr = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    institute = models.CharField(max_length=255)
+    writer_type = models.CharField(max_length=255)
+    writer_number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
