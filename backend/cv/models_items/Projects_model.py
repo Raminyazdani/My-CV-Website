002c1c150700935d64661202adce1a636ff0e623
Ramin <yazdani76ramin@gmail.com>
2024-05-07 14:35:54 +0330

    Ramin | My CV Website | 2024-05-07 14:35 | feature/feature-backend |  Feat: Add Projects model
    This commit introduces the Projects model to manage project-related information:
    
    - Each project is linked to an experience via a ForeignKey, allowing for a structured association with relevant professional experiences.
    - Includes fields for title, start and end dates, and a URL, providing comprehensive details about each project.
    - Enhances the project display and management on the CV website, ensuring each project is well-documented and easily accessible.

---
+++ backend/cv/models_items/Projects_model.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Projects(models.Model):
+    experiences = models.ForeignKey('Experiences', on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
